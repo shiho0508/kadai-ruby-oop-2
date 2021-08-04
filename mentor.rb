88174f5723ac80aa0a21a0b4d2d50720f2a0bf1c
shiho.maeda <shiho0458@gmail.com>
2021-08-04 11:48:25 +0000

    ammend mentor.rb

--- mentor.rb
+++ mentor.rb
@@ -12,18 +12,14 @@ class Mentor
 end
 
 class RailsMentor < Mentor
-    
-#    def initialize
-#        super(mentor_name)
-#    end
         
     def job
-        puts "#{self.name}です。わたしはRubyとRailsでWebアプリケーションを作ります。"
+        puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
     end
 end
 
-kirameki = Mentor.new("kirameki")
-akaide = RailsMentor.new("akaide")
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
 
 kirameki.job
 akaide.job
